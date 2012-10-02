6927e2fb145fa03691944435f77cbfbdb98824bd
Yogesh Dhanapal <geoyogesh@gmail.com>
2012-10-02 11:42:45 +0530

    Added an Startup Project

---
+++ GeoprocessingService/GeoprocessingService/Modules/RootModules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeoprocessingService.Modules
+{
+    public class RootModules:Nancy.NancyModule
+    {
+        public RootModules()
+        {
+            Get["/"]=parameters=>
+            {
+                return "Hello world";
+            };
+        }
+    }
+}
